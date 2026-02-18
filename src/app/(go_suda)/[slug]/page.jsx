ffcfbac22d7bc5de67e6455a282278a5a2d09c5a
Anastasia <smirnova.aa@dns-shop.ru>
2026-02-18 20:17:00 +0300

    mobile ver go suda page

--- src/app/(go_suda)/[slug]/page.jsx
+++ src/app/(go_suda)/[slug]/page.jsx
@@ -1,50 +1,102 @@
+"use client";
+import { useState } from "react";
 import styles from "./go_suda.module.css";
-import { Table } from "antd";
+import { Alert, Button, Layout, Flex, Table } from "antd";
 import Header from "@/components/Header";
+import MobileList from "@/components/MobileList/MobileList";
 import tableData from "@/mocks/go_suda_table.json";
+import useIsMobile from "@/hooks/useIsMobile";
 
 const { people, places } = tableData;
-  const columns = [
-    {
-      title: "",
-      dataIndex: "place",
-      key: "place",
-    },
 
-    ...people.map((person) => ({
-      title: person.name,
-      dataIndex: person.id,
-      key: person.id,
-      align: "center",
-    })),
-  ];
+// Генерация колонок для таблицы
+const columns = [
+  {
+    title: "",
+    dataIndex: "place",
+    key: "place",
+  },
 
-  const dataSource = places.map((place) => {
-    const row = {
-      key: place.id,
-      place: place.name,
-    };
+  ...people.map((person) => ({
+    title: person.name,
+    dataIndex: person.id,
+    key: person.id,
+    align: "center",
+  })),
+];
 
-    people.forEach((person) => {
-      row[person.id] = place.people.includes(person.id) ? "Идёт" : "";
-    });
+// Генерация данных для таблицы
+const dataSource = places.map((place) => {
+  const row = {
+    key: place.id,
+    place: place.name,
+  };
 
-    return row;
+  people.forEach((person) => {
+    row[person.id] = place.people.includes(person.id) ? "Идёт" : "";
   });
 
+  return row;
+});
+
 export default function GoSuda() {
+  const [status, setStatus] = useState(null); // null | 'success' | 'error'
+  const isMobile = useIsMobile();
+
+  // Функция для копирования ссылки в буфер обмена
+  const handleCopy = async () => {
+    const id = window.location.pathname.replace("/", "");
+    const link = `${window.location.origin}/form/${id}`;
+
+    try {
+      await navigator.clipboard.writeText(link);
+      setStatus("success"); // усп
+    } catch (err) {
+      console.error("Ошибка копирования", err);
+      setStatus("error"); // ошибка
+    }
+  };
+
   return (
-    <>
-      <Header title="Го сюда" />
-      <div className={styles.page}>
-        <Table
-          classNames={styles.table}
-          columns={columns}
-          dataSource={dataSource}
-          bordered
-          pagination={false}
-        />
-      </div>
-    </>
+      <Layout style={{ width: "100%", minHeight: "100vh" }}>
+        <Header title="Го сюда" />
+        <Flex className={styles.flexContainer} gap="middle" align="flex-start" justify="center" vertical>
+          {isMobile ? (
+            <MobileList places={places} people={people} />
+          ) : (
+            <div className="table-wrapper">
+              <Table
+                className={styles.table}
+                columns={columns}
+                dataSource={dataSource}
+                bordered
+                pagination={false}
+              />
+            </div>
+          )}
+
+          <Button className={styles.button} type="primary" onClick={handleCopy}>
+            Отправить любимкам
+          </Button>
+
+          {status === "success" && (
+            <Alert
+              title="Скопировано!"
+              description="Ссылка успешно скопирована в буфер обмена."
+              type="success"
+              showIcon
+            />
+          )}
+
+          {status === "error" && (
+            <Alert
+              title="Ошибка"
+              description="Не удалось скопировать ссылку. Попробуйте снова."
+              type="error"
+              showIcon
+            />
+          )}
+        </Flex>
+      </Layout>
   );
 }
---
+++ src/components/MobileList/MobileList.jsx
@@ -0,0 +1,27 @@
+import styles from "./MobileList.module.css";
+
+export default function MobileList({ places, people }) {
+  return (
+    <div className={styles.container}>
+      {places.map((place) => {
+        const goingPeople = people.filter((p) => place.people.includes(p.id));
+
+        return (
+          <div key={place.id} className={styles.card}>
+            <h3 className={styles.cardTitle}>{place.name}</h3>
+
+            {goingPeople.length ? (
+              <ul className={styles.list}>
+                {goingPeople.map((p) => (
+                  <li key={p.id}>{p.name}</li>
+                ))}
+              </ul>
+            ) : (
+              <p className={styles.emptyMessage}>Пока никто не идёт</p>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
