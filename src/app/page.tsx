96de97d89a2b5c221ca74f833c7c77f0c20deb36
milkyss <milkute@yandex.ru>
2026-03-11 23:00:20 +0300

    create InteractiveList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,10 @@
 
 import styles from "./page.module.css";
 import Header from "@/components/Header";
-import Image from "next/image";
 import AppButton from "@/components/Button";
-import {Form, Input, Space} from "antd";
 import React, {useState} from "react";
 import {checkDuplicate} from "@/utils/duplicates";
+import InteractiveList from "@/components/InteractiveList/InteractiveList";
 
 interface ListItem {
     name: string;
@@ -24,7 +23,6 @@ export default function Home() {
     const [placeInputError, setPlaceInputError] = useState('');
 
     function addPerson() {
-        setPersonInputError('');
         const name = personInputName.trim();
 
         if (!name) {
@@ -49,7 +47,6 @@ export default function Home() {
     }
 
     function addPlace() {
-        setPlaceInputError('');
         const newPlace = placeInputName.trim();
 
         if (!newPlace) {
@@ -78,66 +75,38 @@ export default function Home() {
             <Header title={'Го туда'}/>
             <div className={styles.page}>
                 <main className={styles.main}>
-                    <Space vertical style={{ minHeight: '300px' }}>
-                        <Space>
-                            <h2 style={{ margin: 0 }}>
-                                Любимки
-                            </h2>
-                            <Image src={'/heart.png'} alt={''} width={30} height={30}/>
-                        </Space>
-                        <Space style={{gap: 24, marginTop: 10}} align={'start'}>
-                            <Form.Item
-                                validateStatus={personInputError ? 'error' : ''}
-                                help={personInputError}
-                            >
-                                <Input
-                                    value={personInputName}
-                                    onChange={e => setPersonName(e.target.value)}
-                                    onPressEnter={addPerson}
-                                    placeholder={'Имя'}
-                                />
-                            </Form.Item>
-                            <AppButton title={'Добавить'} color={'lilac'} onClick={addPerson}></AppButton>
-                        </Space>
-                        <ul style={{ marginInlineStart: 20 }}>
-                            {persons.map((person) =>
-                                <li key={person.id} style={{paddingBottom: 8, display: 'flex', alignItems: 'center', gap: 12}}>
-                                    <span>{person.name}</span>
-                                    <CloseCircleOutlined onClick={() => removePerson(person.id)} />
-                                </li>
-                            )}
-                        </ul>
-                    </Space>
-                    <Space vertical style={{ minHeight: '300px' }}>
-                        <Space>
-                            <h2 style={{ margin: 0 }}>
-                                Места
-                            </h2>
-                            <Image src={'/star.png'} alt={''} width={30} height={30}/>
-                        </Space>
-                        <Space style={{gap: 24, marginTop: 10}} align={'start'}>
-                            <Form.Item
-                                validateStatus={placeInputError ? 'error' : ''}
-                                help={placeInputError}
-                            >
-                                <Input
-                                    value={placeInputName}
-                                    onChange={e => setPlaceName(e.target.value)}
-                                    onPressEnter={addPlace}
-                                    placeholder={'Название'}
-                                />
-                            </Form.Item>
-                            <AppButton title={'Добавить'} color={'lilac'} onClick={addPlace}></AppButton>
-                        </Space>
-                        <ul style={{ marginInlineStart: 20 }}>
-                            {places.map((place) =>
-                                <li key={place.id} style={{paddingBottom: 8, display: 'flex', alignItems: 'center', gap: 12}}>
-                                    <span>{place.name}</span>
-                                    <CloseCircleOutlined onClick={() => removePlace(place.id)} />
-                                </li>
-                            )}
-                        </ul>
-                    </Space>
+                    <InteractiveList
+                        title="Любимки"
+                        icon="/heart.png"
+                        iconAlt="Иконка сердца"
+                        items={persons}
+                        inputValue={personInputName}
+                        onInputChange={(value) => {
+                            setPersonName(value);
+                            setPersonInputError('');
+                        }}
+                        onAdd={addPerson}
+                        onRemove={removePerson}
+                        inputError={personInputError}
+                        placeholder="Имя"
+                    />
+
+                    <InteractiveList
+                        title="Места"
+                        icon="/star.png"
+                        iconAlt="Иконка звезды"
+                        items={places}
+                        inputValue={placeInputName}
+                        onInputChange={(value) => {
+                            setPlaceName(value);
+                            setPlaceInputError('');
+                        }}
+                        onAdd={addPlace}
+                        onRemove={removePlace}
+                        inputError={placeInputError}
+                        placeholder="Название"
+                        extraButton={<AppButton title="Выбрать на карте" color={'lilac'} />}
+                    />
                     <AppButton size="large" title={'Поехали'} color={'grass'}></AppButton>
                 </main>
             </div>
