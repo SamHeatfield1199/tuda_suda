ba0f01649665a05b9d82181629bb2728db4f642d
milkyss <milkute@yandex.ru>
2026-03-11 22:23:22 +0300

    add places block & duplicates check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,31 +4,89 @@ import styles from "./page.module.css";
 import Header from "@/components/Header";
 import Image from "next/image";
 import AppButton from "@/components/Button";
-import {Input, Space} from "antd";
+import {Form, Input, Space} from "antd";
 import React, {useState} from "react";
 import {CloseCircleOutlined} from "@ant-design/icons";
 
+interface ListItem {
+    name: string;
+    id: string
+}
+
 export default function Home() {
-    const [persons, setPersons] = useState<string[]>([]);
+    const [persons, setPersons] = useState<ListItem[]>([]);
+    const [places, setPlaces] = useState<ListItem[]>([]);
 
     const [personInputName, setPersonName] = useState('');
+    const [placeInputName, setPlaceName] = useState('');
+
+    const [personInputError, setPersonInputError] = useState('');
+    const [placeInputError, setPlaceInputError] = useState('');
+
+    function checkDuplicate <T extends Record<K, string>, K extends keyof T>(
+        value: string,
+        collection: Iterable<T>,
+        key: K,
+    ): boolean {
+        const normalized = value.toLowerCase();
+
+        for (const item of collection) {
+            if (item[key].toLowerCase() === normalized) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     function addPerson() {
+        setPersonInputError('');
         const name = personInputName.trim();
 
         if (!name) {
             return;
         }
 
+        if (checkDuplicate(name, persons, 'name')) {
+            setPersonInputError('Такое имя уже есть в списке');
+
+            return;
+        }
+
         setPersons([
             ...persons,
-            name,
+            { id: crypto.randomUUID(), name },
         ]);
         setPersonName('');
     }
 
-    function removePerson(index: number) {
-        setPersons(persons.filter((name, i) =>  i !== index));
+    function removePerson(id: string) {
+        setPersons(persons.filter((person) => person.id !== id));
+    }
+
+    function addPlace() {
+        setPlaceInputError('');
+        const newPlace = placeInputName.trim();
+
+        if (!newPlace) {
+            return;
+        }
+
+        if (checkDuplicate(newPlace, places, 'name')) {
+            setPlaceInputError('Такое место уже есть в списке');
+
+            return;
+        }
+
+        setPlaces([
+            ...places,
+            { id: crypto.randomUUID(), name: newPlace },
+        ]);
+        setPlaceName('');
+    }
+
+    function removePlace(id: string) {
+        setPlaces(places.filter((place) => place.id !== id));
     }
 
     return (
@@ -36,22 +94,62 @@ export default function Home() {
             <Header title={'Го туда'}/>
             <div className={styles.page}>
                 <main className={styles.main}>
-                    <Space vertical>
+                    <Space vertical style={{ minHeight: '300px' }}>
                         <Space>
                             <h2 style={{ margin: 0 }}>
                                 Любимки
                             </h2>
                             <Image src={'/heart.png'} alt={''} width={30} height={30}/>
                         </Space>
-                        <Space style={{gap: 24, marginTop: 10}}>
-                            <Input value={personInputName} onChange={e => setPersonName(e.target.value)} placeholder={'Имя'} />
+                        <Space style={{gap: 24, marginTop: 10}} align={'start'}>
+                            <Form.Item
+                                validateStatus={personInputError ? 'error' : ''}
+                                help={personInputError}
+                            >
+                                <Input
+                                    value={personInputName}
+                                    onChange={e => setPersonName(e.target.value)}
+                                    onPressEnter={addPerson}
+                                    placeholder={'Имя'}
+                                />
+                            </Form.Item>
                             <AppButton title={'Добавить'} color={'lilac'} onClick={addPerson}></AppButton>
                         </Space>
-                        <ul style={{ marginInlineStart: 20, marginTop: 20 }}>
-                            {persons.map((person, index) =>
-                                <li key={index} style={{paddingBottom: 8, display: 'flex', alignItems: 'center', gap: 12}}>
-                                    <span>{person}</span>
-                                    <CloseCircleOutlined onClick={() => removePerson(index)} />
+                        <ul style={{ marginInlineStart: 20 }}>
+                            {persons.map((person) =>
+                                <li key={person.id} style={{paddingBottom: 8, display: 'flex', alignItems: 'center', gap: 12}}>
+                                    <span>{person.name}</span>
+                                    <CloseCircleOutlined onClick={() => removePerson(person.id)} />
+                                </li>
+                            )}
+                        </ul>
+                    </Space>
+                    <Space vertical style={{ minHeight: '300px' }}>
+                        <Space>
+                            <h2 style={{ margin: 0 }}>
+                                Места
+                            </h2>
+                            <Image src={'/star.png'} alt={''} width={30} height={30}/>
+                        </Space>
+                        <Space style={{gap: 24, marginTop: 10}} align={'start'}>
+                            <Form.Item
+                                validateStatus={placeInputError ? 'error' : ''}
+                                help={placeInputError}
+                            >
+                                <Input
+                                    value={placeInputName}
+                                    onChange={e => setPlaceName(e.target.value)}
+                                    onPressEnter={addPlace}
+                                    placeholder={'Название'}
+                                />
+                            </Form.Item>
+                            <AppButton title={'Добавить'} color={'lilac'} onClick={addPlace}></AppButton>
+                        </Space>
+                        <ul style={{ marginInlineStart: 20 }}>
+                            {places.map((place) =>
+                                <li key={place.id} style={{paddingBottom: 8, display: 'flex', alignItems: 'center', gap: 12}}>
+                                    <span>{place.name}</span>
+                                    <CloseCircleOutlined onClick={() => removePlace(place.id)} />
                                 </li>
                             )}
                         </ul>
